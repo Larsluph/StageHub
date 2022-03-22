5faf7412f0ba9741f1abf41e6e165d53fb8dee25
KwiiKss <lrent.kevin@gmail.com>
2022-03-22 10:20:03 +0100

    SFx8 rechercher une offre

--- sql/queries.sql
+++ sql/queries.sql
@@ -6,6 +6,7 @@ WHERE
 AND
     `users`.`hash` = '$hash';
 
+-- Gestion des entreprises
 -- SFx2 - Rechercher une entreprise
 -- Rechercher une entreprise avec le critère "nom_entreprise"
 SELECT nom_entreprise FROM entreprises WHERE NOT hidden AND nom_entreprise like '%$nom_entreprise%' 
@@ -19,7 +20,7 @@ FROM
 WHERE 
     `secteurs_activite`.`nom_secteur_activite` LIKE '%$secteur_activite%';
 
--- Rechercher une entreprise avec le critère "localites"
+-- Rechercher une entreprise avec le critère "nom_localite"
 SELECT
     `localites`.`nom_localite`, 
     `entreprises`.`nom_entreprise` 
@@ -65,3 +66,66 @@ UPDATE `candidature` SET evaluation = '$evaluation' WHERE id_offre = $id_offre;
 
 
 -- SFx7 - Consulter les statistiques de l'entreprise
+
+-- Gestion des offres de stage
+-- SFx8 - Rechercher une offre
+-- Rechercher une offre avec le critère "nom_competence"
+SELECT
+    `competences`.`nom_competence`, 
+    `offres_stage`.`nom_poste_offre` 
+FROM 
+    `competences` LEFT JOIN `offre_competence` USING (`id_competence`) LEFT JOIN `offres_stage` USING (`id_offre`)
+WHERE 
+    `competences`.`nom_competence` LIKE '%$nom_competence%';
+
+-- Rechercher une offre avec le critère "nom_localite"
+SELECT
+    `localites`.`nom_localite`, 
+    `offres_stage`.`nom_poste_offre` 
+FROM 
+    `localites` LEFT JOIN `offre_loc` USING (`id_localite`) LEFT JOIN `offres_stage` USING (`id_offre`)
+WHERE 
+    `localites`.`nom_localite` LIKE '%$nom_localite%';
+
+-- Rechercher une offre avec le critère "nom_entreprise"
+SELECT
+    `entreprises`.`nom_entreprise`, 
+    `offres_stage`.`nom_poste_offre` 
+FROM 
+    `entreprises` LEFT JOIN `offres_stage` USING (`id_entreprise`)
+WHERE 
+    `entreprises`.`nom_entreprise` LIKE '%$nom_entreprise%';
+
+-- Rechercher une offre avec le critère "duree_stage"
+SELECT 
+	`duree_stage`,`nom_poste_offre`
+FROM 
+    `offres_stage`
+WHERE 
+    `offres_stage`.`duree_stage` like '%$duree_stage%';
+
+-- Rechercher une offre avec le critère "base_remuneration"
+SELECT 
+	`base_remuneration`,`nom_poste_offre`
+FROM 
+    `offres_stage`
+WHERE 
+    `offres_stage`.`base_remuneration` like '%$base_remuneration%';
+
+-- Rechercher une offre avec le critère "date_stage"
+SELECT 
+	`date_stage`,`nom_poste_offre`
+FROM 
+    `offres_stage`
+WHERE 
+    `offres_stage`.`date_stage` like '%$date_stage%';
+
+-- Rechercher une offre avec le critère "nbr_places_offre"
+SELECT 
+	`nbr_places_offre`,`nom_poste_offre`
+FROM 
+    `offres_stage`
+WHERE 
+    `offres_stage`.`nbr_places_offre` like '%$nbr_places_offre%';
+
+-- SFx9 - Créer une offre
