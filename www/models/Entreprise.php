eb8d80aeca7851ecaba8bb48ab6059d2ea2c1cfe
Larsluph <remarso59@sfr.fr>
2022-03-23 21:57:09 +0100

    converted Entreprise from Map to Service

--- www/models/Entreprise.php
+++ www/models/Entreprise.php
@@ -2,28 +2,38 @@
 
 class Entreprise
 {
-    protected int $id_entreprise;
-    protected string $nom_entreprise;
-    protected bool $hidden;
+    private DatabaseConnection $db;
 
-    public function __construct($id, $nom, $hidden) {
-        $this->id_entreprise = $id;
-        $this->nom_entreprise = $nom;
-        $this->hidden = $hidden;
+    public function __construct() {
+        $this->db = new DatabaseConnection(DatabaseConnection::new_connection());
     }
 
-    public function get_id(): int
+    public function create(MapEntreprise $entreprise): bool
     {
-        return $this->id_entreprise;
+        $this->db->execute(
+            "INSERT INTO `entreprises` (`nom_entreprise`, `hidden`) VALUES ('?', '?');",
+            $entreprise->getName(),
+            $entreprise->getHidden()
+        );
+        return true;
     }
 
-    public function get_name(): string
-    {
-        return $this->nom_entreprise;
+    public function read($filter = []) {
+        $query = "SELECT nom_entreprise FROM entreprises WHERE NOT hidden";
+        $params = array();
+        if (array_key_exists("nom",$filter)) {
+            $query .= " AND nom_entreprise like '%?%'";
+            $params[0] = $filter["nom"];
+        }
+
+        $this->db->query($query, ...$params);
     }
 
-    public function is_hidden(): bool
-    {
-        return $this->hidden;
+    public function update(MapEntreprise $entreprise) {
+
+    }
+
+    public function delete(MapEntreprise $entreprise) {
+
     }
 }
