91e44349a3abe77f62a5514ab7d042f14ffc447e
Larsluph <remarso59@sfr.fr>
2022-03-24 12:04:42 +0100

    Entreprise CRUD DONE

--- www/models/Entreprise.php
+++ www/models/Entreprise.php
@@ -17,10 +17,10 @@ class Entreprise
 
     public function create(MapEntreprise $entreprise, MapSecteurActivite $secteurActivite, MapLocalite $localite): bool
     {
-        if ($secteurActivite->getId())
+        if (!$secteurActivite->getId())
             $secteurActivite->setId($this->db->execute("INSERT INTO `secteurs_activite` (nom_secteur_activite) VALUES (?)", $secteurActivite->getName()));
 
-        if ($localite->getId())
+        if (!$localite->getId())
             $localite->setId($this->db->execute("INSERT INTO `localites` (`nom_localite`) VALUES (?)", $localite->getName()));
 
         $this->db->execute(
@@ -35,22 +35,44 @@ class Entreprise
         return true;
     }
 
-    public function read($filter = []) {
-        $query = "SELECT nom_entreprise FROM entreprises WHERE NOT hidden";
+    public function readOne(int $id): MapEntreprise {
+        $row = $this->db->query("SELECT id_entreprise, nom_entreprise, hidden FROM entreprises WHERE id_entreprise=? LIMIT 1", $id)[0];
+        $mapEntreprise = new MapEntreprise();
+        $mapEntreprise->setId((int)$row["id_entrperise"]);
+        $mapEntreprise->setName($row["nom_entreprise"]);
+        $mapEntreprise->setHidden((bool)$row["hidden"]);
+        return $mapEntreprise;
+    }
+
+    public function readAll($filter = []) {
+        $base_query = "SELECT nom_entreprise ";
+        $tables = "FROM entreprises ";
+        $conditions = "WHERE NOT hidden";
+
         $params = array();
+
         if (array_key_exists("nom",$filter)) {
-            $query .= " AND nom_entreprise like '%?%'";
+            $conditions .= " AND nom_entreprise like '%?%'";
             $params[0] = $filter["nom"];
         }
 
-        $this->db->query($query, ...$params);
+        $this->db->query($base_query.$tables.$conditions, ...$params);
     }
 
-    public function update(MapEntreprise $entreprise) {
-
+    public function update(MapEntreprise $entreprise): bool
+    {
+        $this->db->execute(
+            "UPDATE `entreprises` SET nom_entreprise=?, hidden=? WHERE id_entreprise=?",
+            $entreprise->getName(),
+            $entreprise->getHidden(),
+            $entreprise->getId()
+        );
+        return true;
     }
 
     public function delete(MapEntreprise $entreprise) {
-
+        $this->db->execute("DELETE FROM `entreprise_loc` WHERE id_entreprise=?", $entreprise->getId());
+        $this->db->execute("DELETE FROM `entreprise_secteur` WHERE id_entreprise=?", $entreprise->getId());
+        $this->db->execute("DELETE FROM `entreprises` WHERE id_entreprise=?", $entreprise->getId());
     }
 }
