e803f786c3dbc5e220b055e12eb7e3d418f1c01f
Larsluph <remarso59@sfr.fr>
2022-03-25 23:53:54 +0100

    SecteurActivite Model DONE

---
+++ www/App/Models/SecteurActivite.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+
+/**
+ * Localite Model
+ */
+class SecteurActivite extends \Core\Model
+{
+    /**
+     * Get one secteur activite by id
+     * @param int $id id secteur activite
+     * @return array
+     */
+    public static function readOneById(int $id): array
+    {
+        $sql = "SELECT * FROM secteurs_activite WHERE id_secteur_activite = :id";
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    /**
+     * Get one secteur activite by name
+     * @param string $name name secteur activite
+     * @return array
+     */
+    public static function readOneByName(string $name): array
+    {
+        $sql = "SELECT * FROM secteurs_activite WHERE nom_secteur_activite = :name";
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    /**
+     * Get all secteurs activite
+     *
+     * @return array
+     */
+    public static function readAll(): array
+    {
+        $db = static::getDB();
+        $stmt = $db->query("SELECT * FROM secteurs_activite");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Creates a new secteur activite
+     * @param string $name name secteur activite
+     * @return int
+     */
+    public static function create(string $name): int
+    {
+        $sql = "INSERT INTO secteurs_activite (nom_secteur_activite) VALUES (:name)";
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+        $stmt->execute();
+        return $db->lastInsertId();
+    }
+
+    /**
+     * Updates a secteur activite
+     * @param int $id id secteur activite
+     * @param string $name name secteur activite
+     * @return void
+     */
+    public static function update(int $id, string $name): void
+    {
+        $sql = "UPDATE secteurs_activite SET nom_secteur_activite = :name WHERE id_secteur_activite = :id";
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+
+    /**
+     * Deletes a secteur activite
+     * @param  int $id id secteur activite
+     * @return void
+     */
+    public static function delete(int $id): void
+    {
+        $sql = "DELETE FROM secteurs_activite WHERE id_secteur_activite = :id";
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+}
