7675ac1032f2fec58d89b63072e22094edcf07a2
KwiiKss <lrent.kevin@gmail.com>
2022-03-29 09:08:58 +0200

    update w3c

--- www/App/Views/Company_page.php
+++ www/App/Views/Company_page.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="utf-8">
   <title>StageHub</title>
   <link rel="stylesheet" href="/assets/css/style.css">
 </head>
+
 <body>
   <div class="background--custom">
     <canvas id="canvas"></canvas>
   </div>
+
   <script src="/assets/js/background.js"></script>
   <script>
     var gradient = new Gradient();
@@ -21,10 +24,10 @@
       type="button">
       - PROFILE -
     </button>
-    <button class="profile float-end mx-lg-5 mt-5 fs-4" type="button">
+    <button onclick="document.location.href='Apply.html';" class="profile float-end mx-lg-5 mt-5 fs-4" type="button">
       - APPLICATIONS -
     </button>
-    <button class="profile float-end mx-lg-5 mt-5 fs-4" type="button">
+    <button onclick="document.location.href='.html';" class="profile float-end mx-lg-5 mt-5 fs-4" type="button">
       - NEW POST -
     </button>
   </div>
@@ -44,51 +47,54 @@
       <?php foreach ($notifications as $notification ) { ?>
       <div class="notification mt-1 mx-auto col-10">
         <img alt="Logo_notif" class="mx-2 col-1 pt-1 pb-1 float-start" src="/assets/img/notif_ico.ico">
-        <h4 class="fs-6 text-center pt-1 pe-2"><?php echo $notification['title'] ?></h4>
-        <h6 class="fs-6 text-center"><?php echo $notification['content'] ?></h6>
+        <h4 class="fs-6 text-center pt-1 pe-2">Cesi - Engineer School</h4>
+        <h6 class="fs-6 text-center">New student !</h6>
       </div>
       <?php } ?>
       <hr class="col-8 mx-auto">
     </div>
-    <div id="STAGE" class="col-7 mx-auto mt-5 mb-1">
-      <table id="pagination" class="table table-striped table-bordered w-100">
+    <div class="col-7 mx-auto mt-5 mb-1 stage">
+      <table class="pagination table table-striped table-bordered w-100">
         <tbody>
           <tr>
             <td>
               <?php foreach ($offres as $offre) { ?>
               <div class="stage_offer d-flex mb-4">
-                <img alt="Logo_company" class="Logo_company text-center align-self-center mx-4" id="logo"
+                <img alt="Logo_company" class="Logo_company text-center align-self-center mx-4"
                   src="/assets/img/cesi_logo.jpg">
                 <div class="vr align-self-center"></div>
                 <div style="padding-right: 45px;">
-                  <h4 class="mt-4 fs-4 mx-5" value="Name_company"><?php echo $offre['nom_poste_offre'] ?></h4>
+                  <h4 class="mt-4 fs-4 mx-5">
+                    <?php echo $offre['nom_poste_offre'] ?>
+                  </h4>
                   <hr class="col-3 mx-5 mb-4">
                   <table class="col-12 mt-4 mx-5 mb-4">
                     <tr>
-                      <th>
-                        <h5 class="fs-6 fw-bold" value="Description_stage">Quality required :</h4>
-                          <hr class="col-4 mx-1 mb-1">
+                      <th class="fs-6 fw-bold">
+                        Quality required :
+                        <hr class="col-4 mx-1 mb-1">
                       </th>
-                      <th>
-                        <h5 class="fs-6 fw-bold" value="Description_stage">Location :</h4>
-                          <hr class="col-4 mx-1 mb-1">
+                      <th class="fs-6 fw-bold">
+                        Location :
+                        <hr class="col-4 mx-1 mb-1">
                       </th>
                     </tr>
-                    <td>
-                      <h4 class="mt-3 fs-6 mx-auto" value="Name_company"><?php 
-                        echo implode(" - ", explode("|", $offre['competences']));
-                      ?>
-                      </h4>
+                    <tr>
+                      <td class="mt-3 fs-6 mx-auto">
+                        <?php echo implode(" - ", explode("|", $offre['competences'])); ?>
+                      
                     </td>
                     <td>
-                      <h4 class="col-10 mt-3 fs-6" value="Name_company"><?php 
+                      <h4 class="col-10 mt-3 fs-6" value="Name_company">
+                        <?php 
                         echo implode(" - ", explode("|", $offre['localites']));
                       ?>
                       </h4>
                     </td>
                   </table>
-                  <input type="submit" href="#" class="button-register float-start ms-5 px-4 mx-auto mb-3" value="Modify">
-                      
+                  <input type="submit" href="#" class="button-register float-start ms-5 px-4 mx-auto mb-3"
+                    value="Modify">
+
                 </div>
               </div>
               <?php } ?>
@@ -123,8 +129,8 @@
     <div class="Term-of-use-link text-center">
       <div class="copyright text-center p-3">
         © 2022 - StageHub /
-      <a onclick="document.location='/terms'" type="button">Terms of use</a>
-    </div>
+        <a href="/terms">Terms of use</a>
+      </div>
   </footer>
 </body>
 
